5adf9db461223642abc52c58028c67a39057ba9c
Hamm <majhamm@qq.com>
2023-11-30 15:42:38 +0800

    fix(Websocket): 将Websocket由redis迁移至MQTT

--- src/main/java/com/qqlab/spms/module/system/IndexController.java
+++ src/main/java/com/qqlab/spms/module/system/IndexController.java
@@ -3,6 +3,8 @@ package com.qqlab.spms.module.system;
 import cn.hamm.airpower.annotation.Description;
 import cn.hamm.airpower.root.RootController;
 import cn.hamm.airpower.websocket.WebsocketUtil;
+import cn.hutool.core.util.RandomUtil;
+import org.eclipse.paho.client.mqttv3.MqttException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,10 +23,10 @@ public class IndexController extends RootController {
     private WebsocketUtil websocketUtil;
 
     @GetMapping("")
-    public String index() {
-        websocketUtil.sendToAll("Hi guys helloooooo");
-        websocketUtil.sendToUser(1L, "hello 1");
-        websocketUtil.sendToUser(2L, "hello 2");
+    public String index() throws MqttException {
+        websocketUtil.sendToAll("Hi guys helloooooo " + RandomUtil.randomStringUpper(32));
+        websocketUtil.sendToUser(1L, "hello 1 " + RandomUtil.randomStringUpper(32));
+        websocketUtil.sendToUser(2L, "hello 2 " + RandomUtil.randomStringUpper(32));
         return "<h1>Hello World!</h1>";
     }
 }
