ce7ad80952c10d617fd92f30ba15093915509713
Hamm <majhamm@qq.com>
2024-01-29 12:14:26 +0800

    fix(BUG): 优化了一些已知的问题

--- src/main/java/com/qqlab/spms/module/system/permission/PermissionController.java
+++ src/main/java/com/qqlab/spms/module/system/permission/PermissionController.java
@@ -1,14 +1,13 @@
 package com.qqlab.spms.module.system.permission;
 
 import cn.hamm.airpower.annotation.Description;
-import cn.hamm.airpower.query.QueryRequest;
-import cn.hamm.airpower.result.json.JsonData;
 import cn.hamm.airpower.util.TreeUtil;
 import com.qqlab.spms.base.BaseController;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * <h1>Controller</h1>
@@ -33,7 +32,7 @@ public class PermissionController extends BaseController<PermissionEntity, Permi
     }
 
     @Override
-    public JsonData getList(@RequestBody QueryRequest<PermissionEntity> queryRequest) {
-        return jsonData(treeUtil.buildTreeList(service.getList(queryRequest)));
+    protected List<PermissionEntity> afterGetList(List<PermissionEntity> list) {
+        return treeUtil.buildTreeList(list);
     }
 }
