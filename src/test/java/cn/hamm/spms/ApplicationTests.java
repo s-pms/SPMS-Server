27c8d26ca556cfeafe4e6b7c4981c6a0346e599d
Hamm <majhamm@qq.com>
2024-04-24 01:03:19 +0800

    fix(BUG): 修复了一些已知的问题
    Signed-off-by: Hamm <majhamm@qq.com>

--- src/test/java/cn/hamm/spms/ApplicationTests.java
+++ src/test/java/cn/hamm/spms/ApplicationTests.java
@@ -1,10 +1,18 @@
 package cn.hamm.spms;
 
+import cn.hamm.airpower.response.Filter;
+import cn.hamm.airpower.util.ReflectUtil;
+import cn.hamm.spms.base.BaseController;
+import cn.hamm.spms.base.BaseEntity;
 import org.junit.jupiter.api.Test;
+
+import java.lang.reflect.Method;
 
 class ApplicationTests {
     @Test
-    void initTest() {
+    void initTest() throws NoSuchMethodException {
         System.out.println("Hello AirPower!");
+        Method method = BaseController.class.getMethod("getDetail", BaseEntity.class);
+        Filter filter = ReflectUtil.getAnnotation(Filter.class, method);
     }
 }
