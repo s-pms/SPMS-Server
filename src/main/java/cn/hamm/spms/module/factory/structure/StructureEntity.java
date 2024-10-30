773132ffc0fd4e1e1aae1dbb6adec820b8a19e63
Hamm <majhamm@qq.com>
2024-10-30 19:09:27 +0800

    refactor(tree): 重构树形结构相关代码
    - 移除 IServiceTree 接口，将相关方法移至 TreeServiceDelegate
    - 更新 InventoryService、MenuService、PermissionService、StorageService 和 StructureService- 以委托方式使用 TreeServiceDelegate 的方法，减少代码冗余
    - 优化方法参数命名，提高代码可读性

--- src/main/java/cn/hamm/spms/module/factory/structure/StructureEntity.java
+++ src/main/java/cn/hamm/spms/module/factory/structure/StructureEntity.java
@@ -1,17 +1,23 @@
 package cn.hamm.spms.module.factory.structure;
 
 import cn.hamm.airpower.annotation.Description;
-import cn.hamm.spms.base.BaseTreeEntity;
+import cn.hamm.airpower.annotation.Search;
+import cn.hamm.spms.base.BaseEntity;
 import cn.hamm.spms.common.annotation.AutoGenerateCode;
 import cn.hamm.spms.module.system.coderule.CodeRuleField;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
+import jakarta.persistence.Transient;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.experimental.Accessors;
 import org.hibernate.annotations.DynamicInsert;
 import org.hibernate.annotations.DynamicUpdate;
+import org.hibernate.validator.constraints.Length;
+
+import java.util.List;
 
 /**
  * <h1>工厂结构实体</h1>
@@ -26,9 +32,25 @@ import org.hibernate.annotations.DynamicUpdate;
 @DynamicUpdate
 @Table(name = "structure")
 @Description("工厂结构")
-public class StructureEntity extends BaseTreeEntity<StructureEntity> {
+public class StructureEntity extends BaseEntity<StructureEntity> {
     @Description("工厂结构编码")
     @Column(columnDefinition = "varchar(255) default '' comment '工厂结构编码'", unique = true)
     @AutoGenerateCode(CodeRuleField.StructureCode)
     private String code;
+
+    @Description("名称")
+    @Search
+    @Column(columnDefinition = "varchar(255) default '' comment '名称'")
+    @Length(max = 200, message = "名称最多允许{max}个字符")
+    @NotBlank(groups = {WhenUpdate.class, WhenAdd.class}, message = "名称不能为空")
+    private String name;
+
+    @Description("父级ID")
+    @Column(columnDefinition = "bigint UNSIGNED default 0 comment '父级ID'")
+    @Search(Search.Mode.EQUALS)
+    private Long parentId;
+
+    @Description("树子集节点数组")
+    @Transient
+    private List<StructureEntity> children;
 }
