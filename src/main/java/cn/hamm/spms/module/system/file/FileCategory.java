dadf8c4cd9958787ac2b8f402c84d4d26f248288
Hamm <majhamm@qq.com>
2025-02-25 17:26:13 +0800

    refactor(system): 简化文件类别枚举
    - 移除了不必要的文件类别：IMAGE、VIDEO、AUDIO
    - 调整了 TEMP 和 AVATAR 的键值
    - 优化了枚举成员的顺序和注释

--- src/main/java/cn/hamm/spms/module/system/file/FileCategory.java
+++ src/main/java/cn/hamm/spms/module/system/file/FileCategory.java
@@ -13,34 +13,19 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum FileCategory implements IDictionary {
     /**
+     * <h3>临时文件</h3>
+     */
+    TEMP(0, "临时文件"),
+
+    /**
      * <h3>普通文件</h3>
      */
     NORMAL(1, "普通文件"),
 
     /**
-     * <h3>临时文件</h3>
-     */
-    TEMP(2, "临时文件"),
-
-    /**
-     * <h3>图片</h3>
-     */
-    IMAGE(3, "图片"),
-
-    /**
-     * <h3>视频</h3>
-     */
-    VIDEO(4, "视频"),
-
-    /**
-     * <h3>音频</h3>
-     */
-    AUDIO(5, "音频"),
-
-    /**
      * <h3>头像</h3>
      */
-    AVATAR(6, "头像"),
+    AVATAR(1001, "头像"),
     ;
 
     private final int key;
