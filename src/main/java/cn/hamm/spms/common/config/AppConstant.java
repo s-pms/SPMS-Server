bd0c7ed035c40911921980ff7cf2b67a9a22a51a
Hamm <majhamm@qq.com>
2025-05-09 15:15:45 +0800

    refactor(file): 重构文件上传逻辑
    - 移除 AppConfig 中的 uploadAllowExtensions 属性
    - 在 AppConstant 中添加 DEFAULT_EXTENSIONS常量
    - 修改 FileCategory 枚举，增加 extensions 属性
    - 更新 FileService 中的文件类型检查逻辑

--- src/main/java/cn/hamm/spms/common/config/AppConstant.java
+++ src/main/java/cn/hamm/spms/common/config/AppConstant.java
@@ -20,4 +20,16 @@ public class AppConstant {
      * 应用自定义异常代码基数
      */
     public static final int BASE_CUSTOM_ERROR = 200000;
+
+    /**
+     * 默认允许上传的文件后缀
+     */
+    public static final String[] DEFAULT_EXTENSIONS = new String[]{
+            "jpg", "jpeg", "png", "gif", "bmp",
+            "mp4",
+            "mp3", "wav", "wma",
+            "zip", "rar", "7z", "tar", "gz",
+            "pdf", "doc", "docx", "xls", "xlsx",
+            "markdown"
+    };
 }
