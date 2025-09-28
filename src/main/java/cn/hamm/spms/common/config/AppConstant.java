b30138012096d11f77f427839a50f1fc0a541139
Hamm <majhamm@qq.com>
2025-09-29 01:09:59 +0800

    fix(bugs): 修复了一些已知的BUG

--- src/main/java/cn/hamm/spms/common/config/AppConstant.java
+++ src/main/java/cn/hamm/spms/common/config/AppConstant.java
@@ -7,16 +7,6 @@ package cn.hamm.spms.common.config;
  */
 public class AppConstant {
     /**
-     * 版本号key
-     */
-    public static final String APP_VERSION_HEADER = "app-version";
-
-    /**
-     * 平台key
-     */
-    public static final String APP_PLATFORM_HEADER = "app-platform";
-
-    /**
      * 应用自定义异常代码基数
      */
     public static final int BASE_CUSTOM_ERROR = 200000;
