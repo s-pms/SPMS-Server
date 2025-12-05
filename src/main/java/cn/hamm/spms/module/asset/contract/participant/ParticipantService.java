4c138cfb1933c96515575da00322bd2e5d73dc8f
Hamm <majhamm@qq.com>
2025-12-05 21:24:09 +0800

    refactor(contract): 移除合同状态变更逻辑
    - 删除了合同生效方法 enforce
    - 删除了合同终止方法 stop
    - 移除了对 ContractStatus 枚举的依赖
    - 简化了 ParticipantService 类的实现
    
    Signed-off-by: Hamm <majhamm@qq.com>

--- src/main/java/cn/hamm/spms/module/asset/contract/participant/ParticipantService.java
+++ src/main/java/cn/hamm/spms/module/asset/contract/participant/ParticipantService.java
@@ -2,10 +2,7 @@ package cn.hamm.spms.module.asset.contract.participant;
 
 import cn.hamm.spms.base.BaseService;
 import cn.hamm.spms.module.asset.contract.ContractEntity;
-import cn.hamm.spms.module.asset.contract.enums.ContractStatus;
 import org.springframework.stereotype.Service;
-
-import static cn.hamm.airpower.exception.ServiceError.FORBIDDEN;
 
 /**
  * <h1>Service</h1>
@@ -14,27 +11,4 @@ import static cn.hamm.airpower.exception.ServiceError.FORBIDDEN;
  */
 @Service
 public class ParticipantService extends BaseService<ContractEntity, ParticipantRepository> {
-    /**
-     * 生效合同
-     *
-     * @param id 合同ID
-     */
-    public void enforce(long id) {
-        ContractEntity exist = get(id);
-        FORBIDDEN.when(ContractStatus.INVALID.notEqualsKey(exist.getStatus()), "该合同状态无法生效");
-        exist.setStatus(ContractStatus.EFFECTIVE.getKey());
-        update(exist);
-    }
-
-    /**
-     * 终止合同
-     *
-     * @param id 合同ID
-     */
-    public void stop(long id) {
-        ContractEntity exist = get(id);
-        FORBIDDEN.when(ContractStatus.EFFECTIVE.notEqualsKey(exist.getStatus()), "该合同状态无法终止");
-        exist.setStatus(ContractStatus.TERMINATED.getKey());
-        update(exist);
-    }
 }
