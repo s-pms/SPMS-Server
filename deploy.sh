5f91e26622af32d9de1cfe31d4dbaa1ad4777e34
Hamm <majhamm@qq.com>
2026-01-16 15:32:11 +0800

    fix(bugs): 修复了一些已知的BUG

--- deploy.sh
+++ deploy.sh
@@ -1,40 +1,37 @@
-cd /home/server/ && rm -rf ./* && tar zxvf /home/app.tgz -C ./ && rm -f /home/app.tgz
+DOWNLOAD="/home/app.tgz"
+DIR="/home/server/"
 JAR="server-2.0.0.jar"
 CHECK_URL="http://127.0.0.1:8080"
 MAX=100
 SECOND=2
 PROFILE='production'
 
-is_process_running() { ps -p "$1" &>/dev/null; }
-
+cd $DIR && rm -rf ./* && tar zxvf $DOWNLOAD -C ./ && rm -f $DOWNLOAD
+is_running() { ps -p "$1" &>/dev/null; }
 # 停止旧进程
 pids=$(jps | awk -v jar="$JAR" '$2==jar {print $1}')
 for pid in $pids; do
-  echo "STOP PID: $pid"
+  echo "STOP $pid"
   kill -15 "$pid"
-  while is_process_running "$pid"; do
-    echo "Waiting $pid STOP."
+  while is_running "$pid"; do
+    echo "..."
     sleep 1
   done
-  echo "PID $pid STOPPED."
 done
-
 # 启动新进程
-nohup java -jar "$JAR" --spring.profiles.active="$PROFILE" > "/dev/null" 2>&1 &
-echo "$JAR starting."
-
+nohup java -jar "$JAR" --spring.profiles.active="$PROFILE" > /dev/null 2>&1 &
 # 检查启动状态
 INDEX=0
 while [ $INDEX -lt $MAX ]; do
   HTTP_STATUS=$(curl -s -o /dev/null -w "%{http_code}" "$CHECK_URL")
   if [ "$HTTP_STATUS" -eq 200 ]; then
-    echo "APP up!"
+    echo "SERVER UP!"
     exit 0
   fi
-  echo "HTTP $HTTP_STATUS. Retry."
+  echo "$HTTP_STATUS..."
   sleep $SECOND
   INDEX=$((INDEX + 1))
 done
-
-echo "❌ ERROR."
+# 启动失败
+echo "SERVER ERROR."
 exit 1
