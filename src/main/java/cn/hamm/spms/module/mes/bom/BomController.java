eac829429de0deb4b8f7bbb98de705d6bb35789a
Hamm <majhamm@qq.com>
2026-01-20 13:18:04 +0800

    refactor(bom): 优化BOM控制器详情接口实现
    - 添加Json导入以支持返回类型
    - 添加ExposeAll注解以暴露相关实体类
    - 导入MaterialEntity和BomDetailEntity实体类
    - 实现getDetail方法并添加NotNull注解
    - 配置ExposeAll注解包含BomEntity、BomDetailEntity和MaterialEntity

--- src/main/java/cn/hamm/spms/module/mes/bom/BomController.java
+++ src/main/java/cn/hamm/spms/module/mes/bom/BomController.java
@@ -1,8 +1,13 @@
 package cn.hamm.spms.module.mes.bom;
 
+import cn.hamm.airpower.core.Json;
 import cn.hamm.airpower.core.annotation.Description;
+import cn.hamm.airpower.core.annotation.ExposeAll;
 import cn.hamm.airpower.web.api.Api;
 import cn.hamm.spms.base.BaseController;
+import cn.hamm.spms.module.asset.material.MaterialEntity;
+import cn.hamm.spms.module.mes.bom.detail.BomDetailEntity;
+import org.jetbrains.annotations.NotNull;
 
 /**
  * <h1>Controller</h1>
@@ -12,4 +17,9 @@ import cn.hamm.spms.base.BaseController;
 @Api("bom")
 @Description("BOM")
 public class BomController extends BaseController<BomEntity, BomService, BomRepository> {
+    @Override
+    @ExposeAll({BomEntity.class, BomDetailEntity.class, MaterialEntity.class})
+    public Json getDetail(@NotNull BomEntity source) {
+        return super.getDetail(source);
+    }
 }
